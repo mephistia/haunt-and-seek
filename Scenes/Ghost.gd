52580065520a4ef695ce39b5b3c601f84313a150
Julia Stefanello <mephistia.jcs@outlook.com>
2021-04-05 20:32:13 -0300

    Adicionar som de grito do fantasma

--- Scenes/Ghost.gd
+++ Scenes/Ghost.gd
@@ -6,9 +6,13 @@ signal stopped_haunting
 
 var cooldown = 8.0
 
-var duration = 1.5
+var duration = 2.5
 
 var can_haunt = true
+
+var last_sfx_id = -1
+
+export(Array, AudioStream) var boo_sounds: Array
 
 func _ready():
 	$RClickTimer.wait_time = cooldown	
@@ -21,6 +25,7 @@ func _ready():
 	$DetectionArea/DetectionShape.shape.set_height(0)
 	$RClickTimer.connect("timeout", self, "_on_RClickTimer_timeout")
 	$RClickDuration.connect("timeout", self, "_on_RClickDuration_timeout")
+	randomize()
 	.on_ready() # Chamar função pai ("super")
 	
 func _process(delta):
@@ -33,6 +38,7 @@ func _input(event):
 			$RClickTimer.start()
 			$RClickDuration.start()
 			$RClickFeedback.show()
+			rpc("play_random")
 			can_haunt = false
 			rpc("emit_haunting")
 
@@ -51,3 +57,13 @@ sync func emit_haunting():
 	# mudar cor para feedback visual
 	$AnimatedSprite.modulate = Color(1, 0, 0)
 	emit_signal("haunting")
+
+sync func play_random():
+	var rand_id = randi() % boo_sounds.size()
+	while rand_id == last_sfx_id:
+		rand_id = randi() % boo_sounds.size()
+		
+	last_sfx_id = rand_id
+	$SFX.stream = boo_sounds[rand_id]
+	$SFX.play()
+
